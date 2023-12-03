abb03bf891b947a97b2fc8984cd7b4e14b40f869
vampireexec <131703298+vampireexec@users.noreply.github.com>
2023-12-03 18:08:54 -0500

    linear addressing

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -36,7 +36,7 @@ fn main() {
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Part {
     num: i64,
-    locs: Vec<(i64, i64)>,
+    locs: Vec<usize>,
 }
 
 lazy_static! {
@@ -48,37 +48,25 @@ lazy_static! {
     static ref GEAR_RE: Regex = Regex::new(r"\*").unwrap();
 }
 
-trait Grided {
-    fn as_ij(&self, width: i64) -> (i64, i64);
-}
-
-impl Grided for usize {
-    fn as_ij(&self, width: i64) -> (i64, i64) {
-        (*self as i64 % width, *self as i64 / width)
-    }
-}
-
 fn part1() -> Result<(), Box<dyn Error>> {
     let width = INPUT.find("\n").unwrap() as i64 + 1;
-    let mut parts = NUM_RE
+    let parts = NUM_RE
         .find_iter(&INPUT)
         .map(|m| Part {
             num: m.as_str().parse::<i64>().unwrap(),
-            locs: m.range().map(|s| s.as_ij(width)).collect(),
+            locs: m.range().collect(),
         })
         .collect::<Vec<Part>>();
-    let syms: Vec<(i64, i64)> = SYM_RE
-        .find_iter(&INPUT)
-        .map(|m| m.start().as_ij(width))
-        .collect();
+    let syms: Vec<usize> = SYM_RE.find_iter(&INPUT).map(|m| m.start()).collect();
     let mut sum = 0;
-    for part in &mut parts {
-        if part.locs.iter().any(|(i, j)| {
-            let syms = &syms;
-            ADJ.iter()
-                .any(move |(di, dj)| syms.contains(&(i + di, j + dj)))
-        }) {
-            sum += part.num;
+    'search: for part in parts {
+        for s in part.locs {
+            for (di, dj) in ADJ.iter() {
+                if syms.contains(&((s as i64 + di + dj * width) as usize)) {
+                    sum += part.num;
+                    continue 'search;
+                }
+            }
         }
     }
     println!("{}", sum);
@@ -91,22 +79,19 @@ fn part2() -> Result<(), Box<dyn Error>> {
         .find_iter(&INPUT)
         .map(|m| Part {
             num: m.as_str().parse::<i64>().unwrap(),
-            locs: m.range().map(|s| s.as_ij(width)).collect(),
+            locs: m.range().collect(),
         })
         .collect::<Vec<Part>>();
-    let parts_lut: HashMap<(i64, i64), &Part> = parts
+    let parts_lut: HashMap<usize, &Part> = parts
         .iter()
-        .flat_map(|p| p.locs.iter().map(move |l| (*l, p)))
+        .flat_map(|p| p.locs.iter().map(move |s| (*s, p)))
         .collect();
-    let gears: Vec<(i64, i64)> = GEAR_RE
-        .find_iter(&INPUT)
-        .map(|m| m.start().as_ij(width))
-        .collect();
+    let gears: Vec<usize> = GEAR_RE.find_iter(&INPUT).map(|m| m.start()).collect();
     let mut sum = 0;
-    for (i, j) in &gears {
+    for s in &gears {
         let adj: HashSet<&Part> = ADJ
             .iter()
-            .filter_map(|(di, dj)| parts_lut.get(&(i + *di, j + *dj)))
+            .filter_map(|(di, dj)| parts_lut.get(&((*s as i64 + *di + *dj * width) as usize)))
             .cloned()
             .collect();
         if adj.len() == 2 {
