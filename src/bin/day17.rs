50474585680cdea3b3425335558cce1b18ae2bfc
vampireexec <131703298+vampireexec@users.noreply.github.com>
2023-12-17 23:10:03 -0500

    part 1 and 2 done

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use lazy_static::lazy_static;
 use regex::bytes::Regex;
 use std::{
-    collections::{BinaryHeap, HashMap, LinkedList},
+    collections::{BinaryHeap, HashMap},
     fs::read,
     iter::once,
     ops::{Add, AddAssign, Sub},
@@ -89,7 +89,7 @@ impl AddAssign for Point {
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Ord)]
 struct Path {
-    path: LinkedList<Point>,
+    path: Vec<Point>,
     heat: i64,
 }
 
@@ -136,65 +136,42 @@ fn part1() {
 
     let mut queue = BinaryHeap::from([
         Path {
-            path: LinkedList::from([Point::new(1, 0), Point::new(0, 0)]),
+            path: Vec::from([Point::new(1, 0), Point::new(0, 0)]),
             heat: map[0][1],
         },
         Path {
-            path: LinkedList::from([Point::new(0, 1), Point::new(0, 0)]),
+            path: Vec::from([Point::new(0, 1), Point::new(0, 0)]),
             heat: map[1][0],
         },
     ]);
 
     let end = Point::new(map[0].len() as i64 - 1, map.len() as i64 - 1);
     let mut best = Path {
-        path: LinkedList::new(),
+        path: Vec::new(),
         heat: i64::MAX,
     };
-    let mut count = 0;
     let mut visited = HashMap::new();
 
+    let mut counter = 0usize;
     while let Some(path) = queue.pop() {
-        // print!("{} <<< ", path.heat);
-        // println!("{:?}", queue.iter().map(|p| p.heat).collect::<Vec<_>>());
-        let recents = path.path.iter().take(4).cloned().collect::<Vec<_>>();
+        counter += 1;
+        println!("{} (len={}) (count={})", path.heat, queue.len(), counter);
 
-        if path.heat >= best.heat {
-            continue;
-        }
+        let recents = path.path.iter().cloned().collect::<Vec<_>>();
 
         if recents[0] == end {
-            println!("new best {}", count);
-            count += 1;
             best = path;
-            continue;
+            break;
         }
 
         let curr = path.path;
         let heat = path.heat;
+        let adj = find_adj1(&recents);
+        let (from, count) = find_trajectory1(&recents);
+        let cache_key = (recents[0], from, count);
 
-        let arrived_by = recents[0].dir_from(&recents[1]);
-        let mut adj = vec![];
-        if recents.len() < 4
-            || recents[1].dir_from(&recents[2]) != arrived_by
-            || recents[2].dir_from(&recents[3]) != arrived_by
-        {
-            adj.push(arrived_by);
-        }
-        match arrived_by {
-            Dir::Up | Dir::Down => {
-                adj.push(Dir::Left);
-                adj.push(Dir::Right);
-            }
-            Dir::Right | Dir::Left => {
-                adj.push(Dir::Up);
-                adj.push(Dir::Down);
-            }
-        }
-
-        adj.sort();
-        let cache_key = (recents[0], adj.clone());
         if let Some(other) = visited.get(&cache_key) {
-            if *other < heat {
+            if *other <= heat {
                 continue;
             }
         }
@@ -206,13 +183,10 @@ fn part1() {
             if p.i < 0 || p.i as usize >= map[0].len() || p.j < 0 || p.j as usize >= map.len() {
                 continue;
             }
-            if curr.contains(&p) {
-                continue;
-            }
+
             let heat = heat + map[p.j as usize][p.i as usize];
-            let next = once(p)
-                .chain(curr.iter().cloned())
-                .collect::<LinkedList<_>>();
+
+            let next = once(p).chain(curr.iter().cloned()).collect::<Vec<_>>();
             queue.push(Path { path: next, heat });
         }
     }
@@ -230,4 +204,163 @@ fn part1() {
     println!("{:?}", best.heat);
 }
 
-fn part2() {}
+fn find_trajectory1(recents: &[Point]) -> (Dir, usize) {
+    let from = recents[0].dir_from(&recents[1]);
+    let mut curr = recents[0];
+    let mut count = 0;
+    for next in recents.iter().skip(1) {
+        if curr.dir_from(&next) == from {
+            count += 1;
+            curr = *next;
+        } else {
+            break;
+        }
+    }
+    (from, count)
+}
+
+fn find_adj1(recents: &[Point]) -> Vec<Dir> {
+    let arrived_by = recents[0].dir_from(&recents[1]);
+    let mut adj = vec![];
+    if recents.len() < 4
+        || recents[1].dir_from(&recents[2]) != arrived_by
+        || recents[2].dir_from(&recents[3]) != arrived_by
+    {
+        adj.push(arrived_by);
+    }
+
+    match arrived_by {
+        Dir::Up | Dir::Down => {
+            adj.push(Dir::Left);
+            adj.push(Dir::Right);
+        }
+        Dir::Right | Dir::Left => {
+            adj.push(Dir::Up);
+            adj.push(Dir::Down);
+        }
+    }
+
+    adj.sort();
+    adj
+}
+
+fn part2() {
+    let re = Regex::new(r"\d+").unwrap();
+    let toks = &mut re.find_iter(&IN);
+    let map = toks
+        .map(|l| {
+            l.as_bytes()
+                .iter()
+                .map(|d| (d - b'0') as i64)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut queue = BinaryHeap::from([
+        Path {
+            path: Vec::from([Point::new(1, 0), Point::new(0, 0)]),
+            heat: map[0][1],
+        },
+        Path {
+            path: Vec::from([Point::new(0, 1), Point::new(0, 0)]),
+            heat: map[1][0],
+        },
+    ]);
+
+    let end = Point::new(map[0].len() as i64 - 1, map.len() as i64 - 1);
+    let mut best = Path {
+        path: Vec::new(),
+        heat: i64::MAX,
+    };
+    let mut visited = HashMap::new();
+
+    let mut counter = 0usize;
+    while let Some(path) = queue.pop() {
+        counter += 1;
+        println!("{} (len={}) (count={})", path.heat, queue.len(), counter);
+        // println!("{:?}", queue.iter().map(|p| p.heat).collect::<Vec<_>>());
+
+        let recents = path.path.iter().cloned().collect::<Vec<_>>();
+
+        if recents[0] == end {
+            best = path;
+            break;
+        }
+
+        let curr = path.path;
+        let heat = path.heat;
+        let adj = find_adj2(&recents);
+        let (from, count) = find_trajectory2(&recents);
+        let cache_key = (recents[0], from, count);
+
+        if let Some(other) = visited.get(&cache_key) {
+            if *other <= heat {
+                continue;
+            }
+        }
+
+        visited.insert(cache_key, heat);
+
+        for dir in adj {
+            let p = recents[0] + dir.as_point();
+            if p.i < 0 || p.i as usize >= map[0].len() || p.j < 0 || p.j as usize >= map.len() {
+                continue;
+            }
+
+            let heat = heat + map[p.j as usize][p.i as usize];
+
+            let next = once(p).chain(curr.iter().cloned()).collect::<Vec<_>>();
+            queue.push(Path { path: next, heat });
+        }
+    }
+
+    for j in 0..map.len() {
+        for i in 0..map[j].len() {
+            if best.path.contains(&Point::new(i as i64, j as i64)) {
+                print!("*");
+            } else {
+                print!("{}", map[j][i])
+            }
+        }
+        println!("")
+    }
+    println!("{:?}", best.heat);
+}
+
+fn find_trajectory2(recents: &[Point]) -> (Dir, usize) {
+    let from = recents[0].dir_from(&recents[1]);
+    let mut curr = recents[0];
+    let mut count = 0;
+    for next in recents.iter().skip(1) {
+        if curr.dir_from(&next) == from {
+            count += 1;
+            curr = *next;
+        } else {
+            break;
+        }
+    }
+    (from, count)
+}
+
+fn find_adj2(recents: &[Point]) -> Vec<Dir> {
+    let (from, count) = find_trajectory2(recents);
+    let mut adj = vec![];
+    if count < 10 {
+        adj.push(from);
+    }
+    if count >= 4 {
+        match from {
+            Dir::Up | Dir::Down => {
+                adj.push(Dir::Left);
+                adj.push(Dir::Right);
+            }
+            Dir::Right | Dir::Left => {
+                adj.push(Dir::Up);
+                adj.push(Dir::Down);
+            }
+        }
+    }
+
+    adj.sort();
+    adj
+}
